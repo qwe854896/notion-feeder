426b0f05a18b8f3799af6dd3e1e7634025a5a1a2
Parthiv Seetharaman <pachum99@myrdd.info>
2021-11-10 13:43:54 -0800

    Look for existing entries to prevent duplicates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import getNewFeedItems from './feed';
 import {
+  getExistingPages,
   addFeedItemToNotion,
   deleteOldUnreadFeedItemsFromNotion,
 } from './notion';
@@ -7,15 +8,26 @@ import htmlToNotionBlocks from './parser';
 
 async function index() {
   const feedItems = await getNewFeedItems();
+  const existingPages = await getExistingPages(feedItems);
 
   for (let i = 0; i < feedItems.length; i++) {
     const item = feedItems[i];
+
+    const existingEntries = existingPages.find(
+      (page) => page.properties.Link.url === item.link
+    );
+
+    const isNewEntry = existingEntries === undefined;
+
     const notionItem = {
       title: item.title,
       link: item.link,
       content: htmlToNotionBlocks(item.content),
     };
-    await addFeedItemToNotion(notionItem);
+
+    if (isNewEntry) {
+      await addFeedItemToNotion(notionItem);
+    }
   }
 
   await deleteOldUnreadFeedItemsFromNotion();
